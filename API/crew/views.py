4de2a7e05637715731abd5684516bcab2e654371
kushagrasharma-13 <sharmakushagra295@gmail.com>
2024-06-12 11:25:23 +0530

    small changes

--- API/crew/views.py
+++ API/crew/views.py
@@ -1,14 +1,10 @@
-from django.shortcuts import render
-
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.renderers import JSONRenderer
 # Create your views here.
 import os
-import json
 
 from .serializers import CrewMemberSerializer, CrewRequirementSerializer, SelectedCrewSerializer, ProjectSerializer
 from crew.models import CrewMember, Project, CrewRequirement, SelectedCrew
